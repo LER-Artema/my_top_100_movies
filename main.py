af88cedc5f4caa393d1ecbf658e40d277f797da2
Artema <90665250+LER-Artema@users.noreply.github.com>
2022-02-21 16:28:45 -0600

    Fixed the string truncation of max characters

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ class Movie(db.Model):
     id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer(), nullable=False)
-    description = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
     rating = db.Column(db.Float(), nullable=True)
     ranking = db.Column(db.Integer(), nullable=True)
-    review = db.Column(db.String(250), nullable=True)
+    review = db.Column(db.String(500), nullable=True)
     img_url = db.Column(db.String(250), nullable=True)
 
     def __repr__(self):
